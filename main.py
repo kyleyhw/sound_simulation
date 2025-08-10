0e8cf37884f09fe42869770abc96e064949201d3
Kyle <kyleyhw@gmail.com>
2025-08-10 03:46:30 +0100

    2d animation works

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from simulate import Simulate
 from visualize import Visualize
 
-gridsize = (128, 128)
-gridstep = 10
-
-duration = 100
-timestep = 0.01
-
-wavespeed = 330
-
-simulation = Simulate(gridsize=gridsize, gridstep=gridstep, duration=duration, timestep=timestep, wavespeed=wavespeed)
-history = simulation.run()
-
 if __name__ == '__main__':
-    print(history)
+    dims = 2
 
-    visualize = Visualize(history=history)
-    visualize.plot2D(show=True, save=True)
+    if dims == 2:
+        gridsize = (64, 64)
+    if dims == 3:
+        gridsize = (64, 64, 64)
 
+    gridstep = 10
+
+    duration = 40
+    timestep = 0.01
+
+    wavespeed = 330
+
+    simulation = Simulate(gridsize=gridsize, gridstep=gridstep, duration=duration, timestep=timestep,
+                          wavespeed=wavespeed)
+    history = simulation.run()
+
+    isStable = simulation.check_stability()
+    if isStable:
+        print('this is stable')
+    else:
+        print('this is unstable')
+
+    # print(history)
+
+    visualize = Visualize(history=history, params=simulation.get_params())
+    if dims == 2:
+        visualize.plot2D(show=True, save=True)
+    if dims == 3:
+        visualize.plot3D(show=True, save=True)
