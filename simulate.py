8b306573fdcb684009bd7db24882944c82280710
Kyle W <kyleyhw@gmail.com>
2025-08-17 22:40:42 +0100

    changed driver design to make more general, as setup for h5py saving

--- simulate.py
+++ simulate.py
@@ -1,10 +1,10 @@
 import numpy as np
 from tqdm import tqdm
 
+from setup import Driver
 from calculate import Calculate
 from utils import set_edge_values
 laplacian_operator = Calculate().laplacian_operator
-
 
 
 class Simulate:
@@ -19,6 +19,15 @@ class Simulate:
         self.total_iterations = int(np.ceil(duration / timestep)) + 1
         self.history = np.zeros(shape=tuple((self.total_iterations,) + gridsize))
         # self.boundary_conditions = {}
+        self.drivers = []
+
+    def get_params(self):
+        params = {'gridsize' : self.gridsize,
+                  'gridstep' : self.gridstep,
+                  'duration' : self.duration,
+                  'timestep' : self.timestep,
+                  'wavespeed': self.wavespeed}
+        return params
 
     def check_stability(self):
         sigma = self.wavespeed * self.timestep / self.gridstep
@@ -44,7 +53,7 @@ class Simulate:
         return pressure_next
 
     def _apply_initial_conditions(self, pressure):
-        pressure[(1,) * self.dims] += 1 # needs changing
+        # pressure[(1,) * self.dims] += 1 # needs changing
         return pressure
 
     def _apply_boundary_conditions(self, pressure):
@@ -54,15 +63,27 @@ class Simulate:
         pressure = set_edge_values(arr=pressure, value=0)
         return pressure
 
-    def _apply_driver(self, pressure, time):
-        pressure[(1,) * self.dims] += np.cos(2 * np.pi * 3 * time)
+    def add_driver(self, driver:Driver):
+        """Adds a driver object to the simulation."""
+        # Check if the driver location is valid for the grid dimensions
+        if len(driver.location) != self.dims:
+            raise ValueError(f"Driver location has {len(driver.location)} dims, but grid has {self.dims} dims.")
+        if driver.location > self.gridsize:
+            raise ValueError('driver is out of bounds')
+        self.drivers.append(driver)
+        return
+
+    def _apply_drivers(self, pressure, time):
+        for driver in self.drivers:
+            driver_value = driver.get_value(time)
+            pressure[driver.location] += driver_value
         return pressure
 
     def _simulation_loop(self, pressure_prev, pressure_curr, time):
         laplacian = laplacian_operator(grid=pressure_curr, gridstep=self.gridstep)
         pressure_next = self._update(pressure_prev=pressure_prev, pressure_curr=pressure_curr,
                                      laplacian=laplacian)
-        pressure_next = self._apply_driver(pressure=pressure_next, time=time)
+        pressure_next = self._apply_drivers(pressure=pressure_next, time=time)
         pressure_next = self._apply_boundary_conditions(pressure=pressure_next)
         time += self.timestep
         return pressure_curr, pressure_next, time
@@ -81,11 +102,3 @@ class Simulate:
                                                                        pressure_curr=pressure_curr, time=time)
             self.history[iteration] = pressure_curr
         return self.history
-
-    def get_params(self):
-        params = {'gridsize' : self.gridsize,
-                  'gridstep' : self.gridstep,
-                  'duration' : self.duration,
-                  'timestep' : self.timestep,
-                  'wavespeed': self.wavespeed}
-        return params
