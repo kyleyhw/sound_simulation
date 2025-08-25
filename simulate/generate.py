ef19a5bf341e7fc230b7c01186c5ddb945bae151
Kyle <kyleyhw@gmail.com>
2025-08-26 00:19:24 +0100

    added progress bar and removed visualize option

--- simulate/generate.py
+++ simulate/generate.py
@@ -1,9 +1,9 @@
 import numpy as np
 import h5py
 from datetime import datetime
+from tqdm import tqdm
 
 from simulate import Simulate
-from visualize import Visualize
 from setup import GenerateDriver
 from data_io import save_full_simulation_results
 
@@ -25,11 +25,11 @@ if __name__ == '__main__':
 
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    number_of_runs = 1
-    filename = f"simulation_archive_{timestamp}_{number_of_runs}_runs.hdf5"
+    number_of_runs = 3
+    filename = f"simulation_archive_{timestamp}_{number_of_runs}x.hdf5"
 
     with h5py.File('./training_data/'+filename, 'w') as hdf5_file:
-        for i in range(number_of_runs):
+        for run_number in tqdm(range(number_of_runs), desc='generating data'):
             simulation = Simulate(gridsize=gridsize, gridstep=gridstep, duration=duration, timestep=timestep,
                                   wavespeed=wavespeed)
             driver_gen = GenerateDriver(gridsize=gridsize)
@@ -38,17 +38,12 @@ if __name__ == '__main__':
             for i in range(number_of_speakers):
                 driver, driver_params = driver_gen.get_random_cosine(verbose=True)
                 simulation.add_driver(driver=driver)
-                print(driver_params)
 
             simulation.check_stability()
 
             history = simulation.run()
 
-            save_full_simulation_results(simulation_object=simulation, hdf5_file=hdf5_file, simulation_id=1)
+            save_full_simulation_results(simulation_object=simulation, hdf5_file=hdf5_file, simulation_id=run_number)
 
 
-    visualize = False
-    if visualize:
-        params = None
 
-        visualize = Visualize(history=history, params=params).plot2D(show=True)
