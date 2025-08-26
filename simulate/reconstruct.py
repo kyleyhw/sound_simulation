20373901a0dabf79d00d02e2f44a996eee7aab54
Kyle <kyleyhw@gmail.com>
2025-08-26 03:53:21 +0100

    loading raw data works, next step is to reconstruct the simulation instance from the loaded data

---
+++ simulate/reconstruct.py
@@ -0,0 +1,12 @@
+import h5py
+
+from data_io import LoadSimulationResults
+
+filename = 'simulation_archive_2025-08-26_03-01-06_3x_sensor_results.hdf5'
+
+with h5py.File('./training_data/'+filename, 'r') as hdf5_file:
+    print(hdf5_file.keys())
+    for i in range(len(hdf5_file.keys())):
+        simloader = LoadSimulationResults(hdf5_file=hdf5_file)
+        results = simloader.load_raw_results(simulation_id=i)
+        print(results)
