45a82cb6aec1dacef00bd2db52d3922f388fb0b5
Kyle <kyleyhw@gmail.com>
2025-08-26 04:01:16 +0100

    added new sample filenames

--- simulate/reconstruct.py
+++ simulate/reconstruct.py
@@ -2,7 +2,8 @@ import h5py
 
 from data_io import LoadSimulationResults
 
-filename = 'simulation_archive_2025-08-26_03-01-06_3x_sensor_results.hdf5'
+# filename = 'simulation_archive_2025-08-26_03-57-30_3x_full_history.hdf5'
+filename = 'simulation_archive_2025-08-26_03-59-07_3x_sensor_results.hdf5'
 
 with h5py.File('./training_data/'+filename, 'r') as hdf5_file:
     print(hdf5_file.keys())
