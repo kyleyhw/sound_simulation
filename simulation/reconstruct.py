c7ccca7ad28f00f3354ff4be62d8cbf2f0755319
Kyle <kyleyhw@gmail.com>
2025-08-26 04:08:01 +0100

    added if __name__ == '__main__' because reconstruct will most likely contain reconstruction classes later on

--- simulation/reconstruct.py
+++ simulation/reconstruct.py
@@ -2,12 +2,14 @@ import h5py
 
 from data_io import LoadSimulationResults
 
-# filename = 'simulation_archive_2025-08-26_03-57-30_3x_full_history.hdf5'
-filename = 'simulation_archive_2025-08-26_03-59-07_3x_sensor_results.hdf5'
 
-with h5py.File('./training_data/'+filename, 'r') as hdf5_file:
-    print(hdf5_file.keys())
-    for i in range(len(hdf5_file.keys())):
-        simloader = LoadSimulationResults(hdf5_file=hdf5_file)
-        results = simloader.load_raw_results(simulation_id=i)
-        print(results)
+if __name__ == '__main__':
+    # filename = 'simulation_archive_2025-08-26_03-57-30_3x_full_history.hdf5'
+    filename = 'simulation_archive_2025-08-26_03-59-07_3x_sensor_results.hdf5'
+
+    with h5py.File('./training_data/'+filename, 'r') as hdf5_file:
+        print(hdf5_file.keys())
+        for i in range(len(hdf5_file.keys())):
+            simloader = LoadSimulationResults(hdf5_file=hdf5_file)
+            results = simloader.load_raw_results(simulation_id=i)
+            print(results)
