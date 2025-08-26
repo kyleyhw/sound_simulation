59d52a4ff99718f934e7560bb19d90199d3e7331
Kyle <kyleyhw@gmail.com>
2025-08-26 02:47:25 +0100

    incorporated changes in GenerateSensor, SaveSimulationResults. properly saves several runs to a single file with timestamped savefiles

--- simulate/generate.py
+++ simulate/generate.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from tqdm import tqdm
 
 from simulate import Simulate
-from setup import GenerateDriver
-from data_io import save_full_simulation_results
+from setup import GenerateDriver, GenerateSensor
+from data_io import SaveSimulationResults
 
 
 
@@ -22,28 +22,35 @@ if __name__ == '__main__':
 
     wavespeed = 330
 
-
+    save_type = 'full_history'
+    number_of_runs = 3
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    number_of_runs = 3
-    filename = f"simulation_archive_{timestamp}_{number_of_runs}x.hdf5"
+    filename = f"simulation_archive_{timestamp}_{number_of_runs}x_{save_type}.hdf5"
+
 
     with h5py.File('./training_data/'+filename, 'w') as hdf5_file:
+        simsaver = SaveSimulationResults(hdf5_file=hdf5_file)
+        hdf5_file.attrs['save_type'] = save_type
         for run_number in tqdm(range(number_of_runs), desc='generating data'):
             simulation = Simulate(gridsize=gridsize, gridstep=gridstep, duration=duration, timestep=timestep,
                                   wavespeed=wavespeed)
             driver_gen = GenerateDriver(gridsize=gridsize)
+            sensor_gen = GenerateSensor(gridsize=gridsize)
 
             number_of_speakers = np.random.randint(low=1, high=6)
             for i in range(number_of_speakers):
-                driver, driver_params = driver_gen.get_random_cosine(verbose=True)
+                driver = driver_gen.get_random_cosine(detailed=False)
                 simulation.add_driver(driver=driver)
+
+            sensor = sensor_gen.get_random_basic(detailed=False)
+            simulation.add_sensor(sensor=sensor)
 
             simulation.check_stability()
 
             history = simulation.run()
 
-            save_full_simulation_results(simulation_object=simulation, hdf5_file=hdf5_file, simulation_id=run_number)
+            simsaver.save_full_simulation_results(simulation_object=simulation, hdf5_file=hdf5_file, simulation_id=run_number)
 
 
 
