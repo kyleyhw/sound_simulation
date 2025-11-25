6c568c93b34151e2ad170eb3bd6ff9d0e8b156e2
Kyle <kyleyhw@gmail.com>
2025-11-25 17:28:49 +0000

    FIX: Standardize 'location' to 'position' in setup.py

--- src/acoustic_system/simulation/setup.py
+++ src/acoustic_system/simulation/setup.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 from typing import Tuple
 import numpy as np
 
-from waveforms import Waveform, Cosine
-from utils import LocationGenerator
+from .waveforms import Waveform, Cosine
+from .utils import LocationGenerator
 
 
 @dataclass
 class Driver:
-    """A class to hold a location and a Waveform object."""
-    location: Tuple[int, ...]
+    """A class to hold a position and a Waveform object."""
+    position: Tuple[int, ...]
     waveform: Waveform = Cosine
 
     def get_value(self, time):
@@ -17,7 +17,7 @@ class Driver:
 
 @dataclass
 class Sensor:
-    location: Tuple[int, ...]
+    position: Tuple[int, ...]
     timeseries: np.ndarray
     sample_rate: float
 
@@ -25,15 +25,15 @@ class GenerateSensor:
     def __init__(self, gridsize: tuple):
         self.gridsize = gridsize
 
-    def get_random_basic(self, location=None, detailed=False):
-        if not location:
-            location = LocationGenerator(gridsize=self.gridsize).get_new_location()
+    def get_random_basic(self, position=None, detailed=False):
+        if not position:
+            position = LocationGenerator(gridsize=self.gridsize).get_new_location()
 
-        sensor = Sensor(location=location, timeseries=None, sample_rate=None)
+        sensor = Sensor(position=position, timeseries=None, sample_rate=None)
 
         if detailed:
-            print(sensor, (location,))
-            return sensor, (location,)
+            print(sensor, (position,))
+            return sensor, (position,)
         return sensor
 
 
@@ -41,18 +41,18 @@ class GenerateDriver:
     def __init__(self, gridsize: tuple):
         self.gridsize = gridsize
 
-    def get_random_cosine(self, location=None, frequency=None, amplitude=None, detailed=False):
-        if not location:
-            location = LocationGenerator(gridsize=self.gridsize).get_new_location()
+    def get_random_cosine(self, position=None, frequency=None, amplitude=None, detailed=False):
+        if not position:
+            position = LocationGenerator(gridsize=self.gridsize).get_new_location()
         if not frequency:
             frequency = np.random.randint(low=1, high=10, size=1)
         if not amplitude:
             amplitude = np.random.uniform(low=0.5, high=0.9, size=1)
         waveform = Cosine(frequency=frequency, amplitude=amplitude)
 
-        driver = Driver(location=location, waveform=waveform)
+        driver = Driver(position=position, waveform=waveform)
 
         if detailed:
-            print(driver, (location, frequency, amplitude))
-            return driver, (location, frequency, amplitude)
+            print(driver, (position, frequency, amplitude))
+            return driver, (position, frequency, amplitude)
         return driver
